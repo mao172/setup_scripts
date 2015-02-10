18f42f6838b12d167bb36550d04c7687c2919d56
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-02-10 03:42:43 +0000

    add installation script for cloud_conductor_utils (gem)

---
+++ ccu_gem_install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+GEM_NAME=cloud_conductor_utils
+GIT_URL=https://github.com/cloudconductor/cloud_conductor_utils.git
+
+TOOLS_DIR=/opt/chefdk/embedded/bin
+GEM_TOOL=${TOOLS_DIR}/gem
+RAKE_TOOL=${TOOLS_DIR}/rake
+
+WORK_DIR=`pwd`
+
+export PATH=/opt/chefdk/embedded/bin:$PATH
+cd ${WORK_DIR}
+
+${GEM_TOOL} list | grep ${GEM_NAME}
+if [ $? -eq 0 ] ; then
+  exit
+fi
+
+
+ls ./${GEM_NAME}
+if [ $? -ne 0 ] ; then
+  git clone ${GIT_URL}
+fi
+
+cd ${GEM_NAME}/
+git checkout develop
+
+${RAKE_TOOL} build
+${GEM_TOOL} install pkg/${GEM_NAME}-0.0.1.gem
+${GEM_TOOL} list | grep ${GEM_NAME}
+

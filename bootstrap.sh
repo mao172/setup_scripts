8865b7bb2fd63047989d7346709eae97d7d4270e
Masafumi Okamoto <okamoto.masafumi@tis.co.jp>
2015-04-02 06:55:28 +0000

    Modify for running on kitchen-docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ rm -rf ./cloudconductor
 git clone https://github.com/cloudconductor/cloudconductor_init.git
 mv cloudconductor_init cloudconductor
 cd /opt/cloudconductor
-git checkout develop
+git checkout -b deploy 5ca0ae2f80e94f017bab8370190aca19e95cab08
 
 export PATTERN_NAME="${1}"
 export PATTERN_URL="https://github.com/cloudconductor-patterns/${1}.git"
@@ -17,14 +17,9 @@ export PATTERN_REVISION="develop"
 export ROLE="${2}"
 export CONSUL_SECRET_KEY="${3}"
 
-printenv
-
 TOOLS_DIR=/opt/chefdk/embedded/bin
 BUNDLE_TOOL=${TOOLS_DIR}/bundle
 
 ${BUNDLE_TOOL} config build.nokogiri --use-system-libraries
 . /opt/cloudconductor/bin/init.sh
 
-script_dir=`dirname $0`
-. ${script_dir}/curl_config.sh
-. ${script_dir}/consul_webui.sh
